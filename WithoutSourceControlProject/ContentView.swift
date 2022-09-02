4edae696f4b517725b868189394b06cb97ef3182
YacineDeghaies <yasine.deghaies@gmail.com>
2022-09-02 12:07:50 +0200

    Initial Commit

---
+++ WithoutSourceControlProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WithoutSourceControlProject
+//
+//  Created by Mohamed Yassine Deghaies on 02.09.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WithoutSourceControlProject/WithoutSourceControlProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WithoutSourceControlProjectApp.swift
+//  WithoutSourceControlProject
+//
+//  Created by Mohamed Yassine Deghaies on 02.09.22.
+//
+
+import SwiftUI
+
+@main
+struct WithoutSourceControlProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
